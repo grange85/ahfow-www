a01e799ff02c8a872ed31d1035955ea8f79b3ab3
grange85 <andy@grange85.co.uk>
2019-06-08 13:08:08 +0100

    update and deprioritise fb

--- js/ahfow.js
+++ js/ahfow.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	$("article img").addClass("img-responsive");
-    $('a[href^="http://"],a[href^="https://"]')
+
+
+  $('article a[href^="http://"],article a[href^="https://"]')
     	.not('a:has(img)')
         .attr('target','_blank')
         .append(' <i class="fas fa-external-link-alt"></i>')
