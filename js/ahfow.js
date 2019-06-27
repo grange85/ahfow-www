428cdb4e2a9d1065e15e52ec2906ddbc90343356
grange85 <andy@grange85.co.uk>
2019-06-27 18:07:58 +0100

    make things a little less ambiguous

--- js/ahfow.js
+++ js/ahfow.js
@@ -2,9 +2,14 @@ $(document).ready(function() {
 	$("article img").addClass("img-responsive");
 
 
-  $('article a[href^="http://"],article a[href^="https://"]')
+	$('article a[href^="http://"],article a[href^="https://"]')
     	.not('a:has(img)')
         .attr('target','_blank')
-        .append(' <i class="fas fa-external-link-alt"></i>')
-    ;
+        .append(' <i class="fas fa-external-link-alt"></i>');
+
+	$('article a[href*="fuzzywuzzy"]').each(function(){
+		var thisurl = $(this).prop('href');
+		$(this).prop('href', 'https://web.archive.org/web/2007+/' + thisurl);
+		});
+
 });
