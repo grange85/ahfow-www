a0edcbe85fa2f5f5191a7dfae1b262af6f1b527c
grange85 <andy@grange85.co.uk>
2021-02-11 00:10:16 +0000

    fix deployment script

--- deploy.sh
+++ deploy.sh
@@ -16,7 +16,7 @@ source _cloudfront-distribution-id
 bundle exec jekyll build --config _config.yml,_config_build.yml
 mkdir -p _deploy/_admin
 
-if [ -d "../media/" ] 
+if [ -d "/media/xander4/DATA/ahfow/media" ] 
 
 then
 
@@ -26,26 +26,26 @@ then
 
 	# list images
 	echo '<h2 id="images">Images</h2><ul>' >>_deploy/_admin/full-list.html
-	find ../media/ -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.gif' \)| sed -e 's/^\.\{1,2\}\/media//g' -e 's/.*/<li><a href="https:\/\/media.fullofwishes.co.uk&">&<\/a><\/li>/' | sort >> _deploy/_admin/full-list.html
+	find /media/xander4/DATA/ahfow/media/ -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.gif' \)| sed -e 's/^\.\{1,2\}\/media//g' -e 's/.*/<li><a href="https:\/\/media.fullofwishes.co.uk&">&<\/a><\/li>/' | sort >> _deploy/_admin/full-list.html
 	echo '</ul>' >>_deploy/_admin/full-list.html
 	echo "Image list created..."
 
 	# list audio
 	echo '<h2 id="audio">Audio</h2><ul>' >>_deploy/_admin/full-list.html
-	find ../media/ -type f \( -iname '*.mp3' -o -iname '*.m4a' \) | sed -e 's/^\.\{1,2\}\/media//g' -e 's/.*/<li><a href="https:\/\/media.fullofwishes.co.uk&">&<\/a><\/li>/' | sort >> _deploy/_admin/full-list.html
+	find /media/xander4/DATA/ahfow/media/ -type f \( -iname '*.mp3' -o -iname '*.m4a' \) | sed -e 's/^\.\{1,2\}\/media//g' -e 's/.*/<li><a href="https:\/\/media.fullofwishes.co.uk&">&<\/a><\/li>/' | sort >> _deploy/_admin/full-list.html
 	echo '</ul>' >>_deploy/_admin/full-list.html
 	echo "Audio list created..."
 
 	# list video
 	echo '<h2 id="videa">Video</h2><ul>' >>_deploy/_admin/full-list.html
-	find ../media/ -type f \( -iname '*.mov' -o -iname '*.mp4' -o -iname '*.flv' \) | sed -e 's/^\.\{1,2\}\/media//g' -e 's/.*/<li><a href="https:\/\/media.fullofwishes.co.uk&">&<\/a><\/li>/' | sort >> _deploy/_admin/full-list.html
+	find /media/xander4/DATA/ahfow/media/ -type f \( -iname '*.mov' -o -iname '*.mp4' -o -iname '*.flv' \) | sed -e 's/^\.\{1,2\}\/media//g' -e 's/.*/<li><a href="https:\/\/media.fullofwishes.co.uk&">&<\/a><\/li>/' | sort >> _deploy/_admin/full-list.html
 	echo '</ul>' >>_deploy/_admin/full-list.html
 	echo "Video list created..."
 
 
 	# list misc
 	echo '<h2 id="misc">Misc</h2><ul>' >>_deploy/_admin/full-list.html
-	find ../media/ -type f \( -iname '*.zip' -o -iname '*.pdf' -o -iname '*.doc' \) | sed -e 's/^\.\{1,2\}\/media//g' -e 's/.*/<li><a href="https:\/\/media.fullofwishes.co.uk&">&<\/a><\/li>/' | sort >> _deploy/_admin/full-list.html
+	find /media/xander4/DATA/ahfow/media/ -type f \( -iname '*.zip' -o -iname '*.pdf' -o -iname '*.doc' \) | sed -e 's/^\.\{1,2\}\/media//g' -e 's/.*/<li><a href="https:\/\/media.fullofwishes.co.uk&">&<\/a><\/li>/' | sort >> _deploy/_admin/full-list.html
 	echo '</ul>' >>_deploy/_admin/full-list.html
 	echo "Misc list created..."
 
@@ -60,7 +60,7 @@ fi
 # upload to s3
 echo "sync content..."
 # s3cmd sync --guess-mime-type --no-mime-magic --delete-removed --exclude '.sass-cache' --exclude 's3cfg*' --exclude 'database/*' _deploy/ s3://www.fullofwishes.co.uk
-aws s3 sync --size-only --delete --exclude '.sass-cache' --exclude 'database/*' _deploy/ s3://www.fullofwishes.co.uk
+aws s3 sync --size-only --delete --exclude '.sass-cache' _deploy/ s3://www.fullofwishes.co.uk
 
 # copy rss to correct location
 echo "copy rss..."
