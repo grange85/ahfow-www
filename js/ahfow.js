598b1a6e2eef4709d51db018296d8a83c82d1c59
Andy Aldridge <andy@grange85.co.uk>
2023-02-07 13:09:14 +0000

    more tweaks

--- js/ahfow.js
+++ js/ahfow.js
@@ -1,40 +1,9 @@
-$(document).ready(function() {
-
-
-	/* fix old posts for bootstrap 4 */
-	$("article img").each(function(index){
-		if(!$(this).hasClass('img-fluid')){
-		  $(this).addClass('img-fluid');
-		}
-	});
-
-	$("article iframe").each(function(index){
-	if(!$(this).parent('figure').length && !$(this).hasClass('bandcamp')){
-	  $(this).wrap('<figure class="figure embed-responsive"></figure>');
-		if(!$(this).hasClass('embed-responsive-item')){
-		  $(this).addClass('embed-responsive-item');
-		 }
-	 } else {
-	   if (!$(this).parent().hasClass('figure') && !$(this).hasClass('bandcamp')){
-	   	$(this).parent().addClass('figure embed-responsive');
-	   	$(this).addClass('embed-responsive-item');
-	   }
-	 }
-	});
-
-	$('article a[href^="http://"],article a[href^="https://"]')
-    	.not('a:has(img)')
-    	.not('article a[href*="fullofwishes\.co\.uk"]')
-    	.not('article a[href*="web\.archive\.org"]')
-    	.not('article a[data-linktype="background-image-link"]')
-    	.not('.resp-sharing-button__link')
-        .attr({'target': '_blank', 'title': 'external link - opens in a new tab or window'})
-        .append(' <i class="fas fa-external-link-alt"></i>');
-
-
-
-	$('article a[href^="http://web\.archive\.org"],article a[href^="https://web\.archive\.org"]')
-    	.not('a:has(img)')
-        .attr({'target': '_blank', 'title': 'internet archive wayback machine link - opens in a new tab or window'})
-        .append(' <i class="fas fa-arrow-alt-circle-left"></i>');
+window.addEventListener("DOMContentLoaded", function externalLinks() {
+  var anchors = document.getElementsByTagName("a");
+  for (var i = 0; i < anchors.length; i++) {
+    if (anchors[i].hostname !== window.location.hostname) {
+      anchors[i].setAttribute("target", "_blank");
+      anchors[i].setAttribute("rel", "noopener");
+    }
+  }
 });
