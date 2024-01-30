79d5c89a050bc5ae1d49339138ed3f3f9fa53428
Andy Aldridge <andy@grange85.co.uk>
2024-01-30 13:02:03 +0000

    hyvor config

--- assets/js/hyvor-config.js
+++ assets/js/hyvor-config.js
@@ -2,6 +2,17 @@ customElements.whenDefined('hyvor-talk-comments').then(() => {
     const comments = document.createElement("hyvor-talk-comments");
     comments.settings = {
     top_widget: 'none', // 'reactions' | 'ratings' | 'none'
+    editor: {
+        emoji: false,
+        images: false,
+        gifs: false,
+        embeds: false, // link embedding
+        mentions: true,
+        code_blocks: true,
+        blockquotes: true,
+        inline_styles: true, // bold, italic, inline code, strike, spoiler
+        links: true,
+    },
 	}
     // then append
     document.getElementById("wrap").appendChild(comments);
