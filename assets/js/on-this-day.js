eae4ca67769c982c03b16765a6d58dbb9b56df14
Andy Aldridge <andy@grange85.co.uk>
2026-09-02 07:53:10 +0100

    fix js

--- assets/js/on-this-day.js
+++ assets/js/on-this-day.js
@@ -1,5 +1,5 @@
 // On this day
 const d = new Date();
 const mm = String(d.getMonth() + 1).padStart(2, '0');
-const dd = String(d.getDate()).padStart(1, '0');
+const dd = String(d.getDate()).padStart(2, '0');
 document.getElementById('on-this-day-link').href = `/database/on-this-day/${mm}-${dd}/`;
